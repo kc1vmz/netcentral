cbbf0edb99982af39548c6fe2aafe8177973189d
John Rokicki <jcrokicki@gmail.com>
2026-01-24 22:05:03 -0500

    pull together all artifacts for Github upload, add documents to it

---
+++ build_results/gather.bat
@@ -0,0 +1,14 @@
+@echo off
+rd /s/q target
+mkdir target
+echo Gathering files for upload to Github
+
+copy ..\docker\target\netcentral-docker-image-build-scripts*.zip target
+copy ..\netcentral-server\target\netcentral-server*.jar target
+copy ..\transceivers\transceiver-agw\target\transceiver-agw*.jar target
+copy ..\transceivers\transceiver-aprsis\target\transceiver-aprsis*.jar target
+copy ..\transceivers\transceiver-kenwood\target\transceiver-kenwood*.jar target
+copy ..\transceivers\transceiver-kiss\target\transceiver-kiss*.jar target
+copy ..\transceivers\transceiver-test\target\transceiver-test*.jar target
+copy ..\ui\net-central-ui\target\netcentral-ui*.zip target
+copy ..\documents\target\netcentral-documents*.zip target
