6720d3f1332afca60ced618a0d57f31a104e53d2
John Rokicki <jcrokicki@gmail.com>
2026-05-11 18:10:51 -0400

    version 1.0.19

--- installers/docker/split/publish.bat
+++ installers/docker/split/publish.bat
@@ -1,10 +1,10 @@
-docker tag netcentral-server:1.0.18 kc1vmz/netcentral-server:1.0.18
-docker tag netcentral-ui:1.0.18 kc1vmz/netcentral-ui:1.0.18
-docker tag netcentral-transceiver-aprsis:1.0.18 kc1vmz/netcentral-transceiver-aprsis:1.0.18
-docker tag netcentral-transceiver-kenwood:1.0.18 kc1vmz/netcentral-transceiver-kenwood:1.0.18
-docker tag netcentral-transceiver-kiss:1.0.18 kc1vmz/netcentral-transceiver-kiss:1.0.18
-docker push kc1vmz/netcentral-server:1.0.18
-docker push kc1vmz/netcentral-ui:1.0.18
-docker push kc1vmz/netcentral-transceiver-aprsis:1.0.18
-docker push kc1vmz/netcentral-transceiver-kenwood:1.0.18
-docker push kc1vmz/netcentral-transceiver-kiss:1.0.18
+docker tag netcentral-server:1.0.19 kc1vmz/netcentral-server:1.0.19
+docker tag netcentral-ui:1.0.19 kc1vmz/netcentral-ui:1.0.19
+docker tag netcentral-transceiver-aprsis:1.0.19 kc1vmz/netcentral-transceiver-aprsis:1.0.19
+docker tag netcentral-transceiver-kenwood:1.0.19 kc1vmz/netcentral-transceiver-kenwood:1.0.19
+docker tag netcentral-transceiver-kiss:1.0.19 kc1vmz/netcentral-transceiver-kiss:1.0.19
+docker push kc1vmz/netcentral-server:1.0.19
+docker push kc1vmz/netcentral-ui:1.0.19
+docker push kc1vmz/netcentral-transceiver-aprsis:1.0.19
+docker push kc1vmz/netcentral-transceiver-kenwood:1.0.19
+docker push kc1vmz/netcentral-transceiver-kiss:1.0.19
